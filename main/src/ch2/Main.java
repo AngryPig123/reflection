e226f186221b87be632519815a854c9ee4d72cc6
Angrypig123 <shguddnr3@naver.com>
2024-02-07 12:53:05 +0900

    class constructor relection

--- main/src/ch2/Main.java
+++ main/src/ch2/Main.java
@@ -1,22 +1,45 @@
 package ch2;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Main {
 
     //  Class.getDe
 
-    public static void main(String[] args) {
-        printConstructorData(Person.class);
+    public static void main(String[] args) throws InvocationTargetException, InstantiationException, IllegalAccessException {
+
+        printConstructorData(Person.class); //  생성자 목록 확인
+
+        Person noArgumentConstructors = createInstanceWithArguments(Person.class);
+        System.out.println(noArgumentConstructors);
+
+        Address address = createInstanceWithArguments(Address.class, "동작구", 104);
+        System.out.println(address);
+
+        Person allArgumentConstructors = createInstanceWithArguments(Person.class, address, "홍길동", 20);
+        System.out.println(allArgumentConstructors);
+
+
+    }
+
+    public static <T> T createInstanceWithArguments(Class<T> clazz, Object... objects) throws InvocationTargetException, InstantiationException, IllegalAccessException {
+
+        for (Constructor<?> constructor : clazz.getDeclaredConstructors()) {    //  선언된 생성자를 가져온다.
+            if (constructor.getParameterTypes().length == objects.length) { //  생성자의 필드 수와 인자의 길이를 비교해서 맞는 생성자 클래스를 반환한다, getParameterTypes(), getTypeParameters() * 주의
+                return (T) constructor.newInstance(objects);
+            }
+        }
+        System.out.println("not found");
+        return null;
     }
 
     public static void printConstructorData(Class<?> clazz) {
         Constructor<?>[] constructors = clazz.getDeclaredConstructors();    //  클래스에 정의된 모든 생성자를 가져온다.
 
-        System.out.println(String.format("class %s has %d declared constructors", clazz.getSimpleName(), constructors.length));
+        System.out.printf("class %s has %d declared constructors%n", clazz.getSimpleName(), constructors.length);
 
         for (int i = 0; i < constructors.length; i++) { //  정의된 생성자의 수 만큼 for 문을 돌린다.
 
@@ -40,27 +63,36 @@ public class Main {
         private final int age;
 
         public Person() {
-            this.address = null;
             this.name = "anonymous";
             this.age = 0;
+            this.address = null;
         }
 
         public Person(String name) {
-            this.address = null;
             this.name = name;
             this.age = 0;
+            this.address = null;
         }
 
         public Person(String name, int age) {
-            this.address = null;
             this.name = name;
             this.age = age;
+            this.address = null;
         }
 
         public Person(Address address, String name, int age) {
-            this.address = address;
             this.name = name;
             this.age = age;
+            this.address = address;
+        }
+
+        @Override
+        public String toString() {
+            return "Person{" +
+                    "address=" + address +
+                    ", name='" + name + '\'' +
+                    ", age=" + age +
+                    '}';
         }
 
     }
@@ -73,6 +105,15 @@ public class Main {
             this.street = street;
             this.number = number;
         }
+
+        @Override
+        public String toString() {
+            return "Address{" +
+                    "street='" + street + '\'' +
+                    ", number=" + number +
+                    '}';
+        }
+
     }
 
 }
